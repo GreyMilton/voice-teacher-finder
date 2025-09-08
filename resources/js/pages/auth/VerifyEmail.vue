4e3e0c245698bfe04e9c9fbe8ad1b898e51fdf92
Grey Milton <grey.milton@gmnail.com>
2025-09-09 05:21:02 +0930

    Configure Prettier and apply formatting.

--- resources/js/pages/auth/VerifyEmail.vue
+++ resources/js/pages/auth/VerifyEmail.vue
@@ -8,25 +8,48 @@ import { Form, Head } from '@inertiajs/vue3';
 import { LoaderCircle } from 'lucide-vue-next';
 
 defineProps<{
-    status?: string;
+  status?: string;
 }>();
 </script>
 
 <template>
-    <AuthLayout title="Verify email" description="Please verify your email address by clicking on the link we just emailed to you.">
-        <Head title="Email verification" />
+  <AuthLayout
+    title="Verify email"
+    description="Please verify your email address by clicking on the link we just emailed to you."
+  >
+    <Head title="Email verification" />
 
-        <div v-if="status === 'verification-link-sent'" class="mb-4 text-center text-sm font-medium text-green-600">
-            A new verification link has been sent to the email address you provided during registration.
-        </div>
+    <div
+      v-if="status === 'verification-link-sent'"
+      class="mb-4 text-center text-sm font-medium text-green-600"
+    >
+      A new verification link has been sent to the email address you provided
+      during registration.
+    </div>
 
-        <Form v-bind="EmailVerificationNotificationController.store.form()" class="space-y-6 text-center" v-slot="{ processing }">
-            <Button :disabled="processing" variant="secondary">
-                <LoaderCircle v-if="processing" class="h-4 w-4 animate-spin" />
-                Resend verification email
-            </Button>
+    <Form
+      v-bind="EmailVerificationNotificationController.store.form()"
+      class="space-y-6 text-center"
+      v-slot="{ processing }"
+    >
+      <Button
+        :disabled="processing"
+        variant="secondary"
+      >
+        <LoaderCircle
+          v-if="processing"
+          class="h-4 w-4 animate-spin"
+        />
+        Resend verification email
+      </Button>
 
-            <TextLink :href="logout()" as="button" class="mx-auto block text-sm"> Log out </TextLink>
-        </Form>
-    </AuthLayout>
+      <TextLink
+        :href="logout()"
+        as="button"
+        class="mx-auto block text-sm"
+      >
+        Log out
+      </TextLink>
+    </Form>
+  </AuthLayout>
 </template>

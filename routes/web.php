9aa88d4242f7638134e25ee9016825d481a42eaa
Grey Milton <grey.milton@gmail.com>
2025-09-09 05:57:36 +0930

    Establish routes for guest pages.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,30 @@
 <?php
 
+use App\Models\Teacher;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
+
+Route::get('/teachers', function () {
+    return Inertia::render('TeacherIndex');
+})->name('teachers');
+
+Route::get('/teachers/{teacher}', function (Teacher $teacher) {
+    return Inertia::render('TeacherShow', [
+        'teacher' => $teacher,
+    ]);
+})->name('search');
+
+Route::get('/faqs', function () {
+    return Inertia::render('FaqsPage');
+})->name('faqs');
+
+Route::get('/contact', function () {
+    return Inertia::render('ContactUs');
+})->name('contact');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');

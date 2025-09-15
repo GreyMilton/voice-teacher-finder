a0290fa7f0d97eac28545c16f6d7d1e1290b631f
Grey Milton <grey.milton@gmail.com>
2025-09-16 05:50:27 +0930

    Write a basic feature test for home page.

---
+++ tests/Feature/WelcomeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\User;
+
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
+
+test('guests can visit the welcome page', function () {
+    $response = $this->get(route('home'));
+    $response->assertStatus(200);
+});
+
+test('authenticated users can visit the welcome page', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $response = $this->get(route('home'));
+    $response->assertStatus(200);
+});

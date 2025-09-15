8689b956697b16a610291cbe92aa534d157ad432
Grey Milton <grey.milton@gmnail.com>
2025-09-16 05:55:51 +0930

    Make home page references consistent as 'home' rather than 'welcome' across the code base.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('HomePage');
 })->name('home');
 
 Route::get('/teachers', function () {

3e288e8cbf4bb6ab07a23d5bbdb77576d132a878
Grey Milton <grey.milton@gmnail.com>
2025-09-23 20:57:56 +0930

    Run pint: - `./vendor/bin/pint`

--- app/Models/TeacherUpdateCohort.php
+++ app/Models/TeacherUpdateCohort.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
  * @property int $update_cohort_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|TeacherUpdateCohort newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|TeacherUpdateCohort newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|TeacherUpdateCohort query()
@@ -18,6 +19,7 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|TeacherUpdateCohort whereTeacherId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|TeacherUpdateCohort whereUpdateCohortId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|TeacherUpdateCohort whereUpdatedAt($value)
+ *
  * @mixin \Eloquent
  */
 class TeacherUpdateCohort extends Pivot

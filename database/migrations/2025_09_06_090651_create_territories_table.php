b70cb57feafb0ab89eb66856661adf080540f05d
Grey Milton <grey.milton@gmnail.com>
2025-09-25 20:26:09 +0930

    Use a full data set of territories to populate database on initial migration rather than seeding with a factory.

--- database/migrations/2025_09_06_090651_create_territories_table.php
+++ database/migrations/2025_09_06_090651_create_territories_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Territory;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +14,20 @@ return new class extends Migration
     {
         Schema::create('territories', function (Blueprint $table) {
             $table->id();
+            $table->string('geo_point');
+            $table->string('iso_3_country_code');
             $table->string('english_name');
-            $table->string('local_name');
+            $table->string('continent');
+            $table->string('region');
+            $table->string('french_name');
             $table->timestamps();
         });
+
+        $territories = require database_path('initial_data/territories.php');
+
+        foreach ($territories as $territory) {
+            Territory::create($territory);
+        }
     }
 
     /**

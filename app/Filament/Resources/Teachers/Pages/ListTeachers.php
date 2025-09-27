8d85570847b3f76bdbc548c0c4c58a3186427732
Grey Milton <grey.milton@gmnail.com>
2025-09-27 21:36:04 +1000

    Show tabs for the teachers table in admin panel. Three tabs: all, visible, and hidden.

--- app/Filament/Resources/Teachers/Pages/ListTeachers.php
+++ app/Filament/Resources/Teachers/Pages/ListTeachers.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Teachers\Pages;
 use App\Filament\Resources\Teachers\TeacherResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTeachers extends ListRecords
 {
@@ -14,6 +16,19 @@ class ListTeachers extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'visible' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->visible()),
+            'hidden' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->whereNot(fn (Builder $query) => $query->visible())
+                ),
         ];
     }
 }

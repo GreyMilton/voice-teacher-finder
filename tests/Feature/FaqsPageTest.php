e63b86ee77a319f0dcf8a8f2df3f2c361119e22e
Grey Milton <grey.milton@gmail.com>
2025-09-29 21:52:54 +1000

    Remove unnecessary `$response` variable in two tests in FaqsPageTest.

--- tests/Feature/FaqsPageTest.php
+++ tests/Feature/FaqsPageTest.php
@@ -5,14 +5,12 @@ use App\Models\User;
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
 test('guests can visit the faqs page', function () {
-    $response = $this->get(route('faqs'));
-    $response->assertStatus(200);
+    $this->get(route('faqs'))
+        ->assertStatus(200);
 });
 
 test('authenticated users can visit the faqs page', function () {
-    $user = User::factory()->create();
-    $this->actingAs($user);
-
-    $response = $this->get(route('faqs'));
-    $response->assertStatus(200);
+    $this->actingAs(User::factory()->create())
+        ->get(route('faqs'))
+        ->assertStatus(200);
 });

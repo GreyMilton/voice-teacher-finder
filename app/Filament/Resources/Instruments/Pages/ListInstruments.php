9c2775d7104e0c85c82048da3e1ce7bc41006759
Grey Milton <grey.milton@gmail.com>
2025-10-07 22:01:04 +1100

    Set up tabs on instruments table in admin panel - by teachers associated with each instrument.

--- app/Filament/Resources/Instruments/Pages/ListInstruments.php
+++ app/Filament/Resources/Instruments/Pages/ListInstruments.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Instruments\Pages;
 use App\Filament\Resources\Instruments\InstrumentResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListInstruments extends ListRecords
 {
@@ -14,6 +16,17 @@ class ListInstruments extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'all in use' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->has('teachers')
+                ),
         ];
     }
 }

f3a95ea6880dc722fa61947330cc0942417560ae
Grey Milton <grey.milton@gmail.com>
2025-10-07 22:10:15 +1100

    Set up tabs on territories table in admin panel - by teachers associated with each territory.

--- app/Filament/Resources/Territories/Pages/ListTerritories.php
+++ app/Filament/Resources/Territories/Pages/ListTerritories.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Territories\Pages;
 use App\Filament\Resources\Territories\TerritoryResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTerritories extends ListRecords
 {
@@ -14,6 +16,33 @@ class ListTerritories extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'all in use' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->where(fn (Builder $query) => $query
+                        ->has('teachersFrom')
+                        ->orHas('teachersResiding')
+                        ->orHas('teachersWhoTeachIn')
+                    )
+                ),
+            'have teachers teaching in' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->has('teachersWhoTeachIn')
+                ),
+            'have teachers from' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->has('teachersFrom')
+                ),
+            'have teachers residing' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->has('teachersFrom')
+                ),
         ];
     }
 }

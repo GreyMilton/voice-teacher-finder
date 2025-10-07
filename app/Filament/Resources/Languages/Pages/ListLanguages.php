d6b5a92e7465d7c9ef9d2668334299b157c4d058
Grey Milton <grey.milton@gmail.com>
2025-10-07 21:58:39 +1100

    Set up tabs on languages table in admin panel - by teachers associated with each language.

--- app/Filament/Resources/Languages/Pages/ListLanguages.php
+++ app/Filament/Resources/Languages/Pages/ListLanguages.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Languages\Pages;
 use App\Filament\Resources\Languages\LanguageResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListLanguages extends ListRecords
 {
@@ -14,6 +16,28 @@ class ListLanguages extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'all in use' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->where(fn (Builder $query) => $query
+                        ->has('teachersWhoTeachIn')
+                        ->orHas('teachersWhoSing')
+                    )
+                ),
+            'tuition languages' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->has('teachersWhoTeachIn')
+                ),
+            'sung languages' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->has('teachersWhoSing')
+                ),
         ];
     }
 }

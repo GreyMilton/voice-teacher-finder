b03877f373c2708a83ea9b7ad6f636d3987978a5
Grey Milton <grey.milton@gmail.com>
2025-10-07 21:45:02 +1100

    Set up tabs on cohorts table in admin panel - by cohort type.

--- app/Filament/Resources/Cohorts/Pages/ListCohorts.php
+++ app/Filament/Resources/Cohorts/Pages/ListCohorts.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\Cohorts\Pages;
 
+use App\Enums\CohortType;
 use App\Filament\Resources\Cohorts\CohortResource;
+use App\Models\Cohort;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListCohorts extends ListRecords
 {
@@ -14,6 +18,23 @@ class ListCohorts extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'Initial Authorisation' => Tab::make()
+                ->modifyQueryUsing(function (Builder $query) {
+                    /** @var Builder<Cohort> $query */
+                    $query->whereCohortType(CohortType::InitialAuthorisation);
+                }),
+            'Update' => Tab::make()
+                ->modifyQueryUsing(function (Builder $query) {
+                    /** @var Builder<Cohort> $query */
+                    $query->whereCohortType(CohortType::Update);
+                }),
         ];
     }
 }
